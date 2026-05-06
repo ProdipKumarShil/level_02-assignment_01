157aaff23a1f235f7d060fe2016f8b4534747f0d
ProdipKumarShil <prodipkrishna01@gmail.com>
2026-05-06 23:53:42 +0600

    Solved - 02

--- solutions.ts
+++ solutions.ts
@@ -2,4 +2,15 @@ const filterEvenNumbers = (arr: number[]): number[] => {
   return arr.filter((num: number) => num % 2 == 0)
 }
 
-console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]))
+// console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]))
+
+
+/**
+ * Problem - 02
+ */
+
+const reverseString = (funcStr: string): string => {
+  return funcStr.split('').reverse().join('')
+}
+
+console.log(reverseString('prodip'))

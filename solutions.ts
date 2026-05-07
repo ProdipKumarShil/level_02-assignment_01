200794d8e60b1354af6dbe92fc0cf30c46e99566
ProdipKumarShil <prodipkrishna01@gmail.com>
2026-05-07 22:46:38 +0600

    Solved - Problem 06

--- solutions.ts
+++ solutions.ts
@@ -28,5 +28,36 @@ const checkType = (param: StringOrNumber): string => {
   }
 }
 
-console.log(checkType(23))
-console.log(checkType('is'))
+// console.log(checkType(23))
+// console.log(checkType('is'))
+
+/**
+ * Problem - 06
+ */
+
+class Person{
+  name: string;
+  age: number
+
+  constructor(name: string, age: number){
+    this.name = name,
+    this.age = age
+  }
+}
+
+class Student extends Person{
+  grade: string;
+
+  constructor(name: string, age: number, grade: string){
+    super(name, age)
+    this.grade = grade
+  }
+  getDetails (){
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`
+  }
+}
+
+// const student = new Student("Alice", 20, "A");
+// const student1 = new Student("saber", 55, "C");
+
+// console.log(student.getDetails())
